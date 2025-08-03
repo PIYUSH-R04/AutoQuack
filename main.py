cddae135e1d40049e63bb7bda61d4c02ef22cda1
Lone Wolf <pr099rimalpiyush@gmail.com>
2025-08-04 02:16:41 +0545

    playing around readme

--- main.py
+++ main.py
@@ -17,17 +17,26 @@ from payloads.recon import get_payload as get_recon
 from payloads.cred_dump import get_payload as get_cred_dump
 from payloads.persistence import get_payload as get_persistence
 
-BANNER = r"""
-
- ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░      ░▒▓███████▓▒░  
-░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░   ░▒▓█▓▒░   ░▒▓██████▓▒░ ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░ 
-                                                      ░▒▓█▓▒░                                                         
-                                                       ░▒▓██▓▒░                                                       
+BANNER = r"""                                                                                                                                                                             
+                                                                                                                                                                                   
+               AAA                                     tttt                                QQQQQQQQQ                                                            kkkkkkkk           
+              A:::A                                 ttt:::t                              QQ:::::::::QQ                                                          k::::::k           
+             A:::::A                                t:::::t                            QQ:::::::::::::QQ                                                        k::::::k           
+            A:::::::A                               t:::::t                           Q:::::::QQQ:::::::Q                                                       k::::::k           
+           A:::::::::A        uuuuuu    uuuuuuttttttt:::::ttttttt       ooooooooooo   Q::::::O   Q::::::Quuuuuu    uuuuuu    aaaaaaaaaaaaa      cccccccccccccccc k:::::k    kkkkkkk
+          A:::::A:::::A       u::::u    u::::ut:::::::::::::::::t     oo:::::::::::oo Q:::::O     Q:::::Qu::::u    u::::u    a::::::::::::a   cc:::::::::::::::c k:::::k   k:::::k 
+         A:::::A A:::::A      u::::u    u::::ut:::::::::::::::::t    o:::::::::::::::oQ:::::O     Q:::::Qu::::u    u::::u    aaaaaaaaa:::::a c:::::::::::::::::c k:::::k  k:::::k  
+        A:::::A   A:::::A     u::::u    u::::utttttt:::::::tttttt    o:::::ooooo:::::oQ:::::O     Q:::::Qu::::u    u::::u             a::::ac:::::::cccccc:::::c k:::::k k:::::k   
+       A:::::A     A:::::A    u::::u    u::::u      t:::::t          o::::o     o::::oQ:::::O     Q:::::Qu::::u    u::::u      aaaaaaa:::::ac::::::c     ccccccc k::::::k:::::k    
+      A:::::AAAAAAAAA:::::A   u::::u    u::::u      t:::::t          o::::o     o::::oQ:::::O     Q:::::Qu::::u    u::::u    aa::::::::::::ac:::::c              k:::::::::::k     
+     A:::::::::::::::::::::A  u::::u    u::::u      t:::::t          o::::o     o::::oQ:::::O  QQQQ:::::Qu::::u    u::::u   a::::aaaa::::::ac:::::c              k:::::::::::k     
+    A:::::AAAAAAAAAAAAA:::::A u:::::uuuu:::::u      t:::::t    tttttto::::o     o::::oQ::::::O Q::::::::Qu:::::uuuu:::::u  a::::a    a:::::ac::::::c     ccccccc k::::::k:::::k    
+   A:::::A             A:::::Au:::::::::::::::uu    t::::::tttt:::::to:::::ooooo:::::oQ:::::::QQ::::::::Qu:::::::::::::::uua::::a    a:::::ac:::::::cccccc:::::ck::::::k k:::::k   
+  A:::::A               A:::::Au:::::::::::::::u    tt::::::::::::::to:::::::::::::::o QQ::::::::::::::Q  u:::::::::::::::ua:::::aaaa::::::a c:::::::::::::::::ck::::::k  k:::::k  
+ A:::::A                 A:::::Auu::::::::uu:::u      tt:::::::::::tt oo:::::::::::oo    QQ:::::::::::Q    uu::::::::uu:::u a::::::::::aa:::a cc:::::::::::::::ck::::::k   k:::::k 
+AAAAAAA                   AAAAAAA uuuuuuuu  uuuu        ttttttttttt     ooooooooooo        QQQQQQQQ::::QQ    uuuuuuuu  uuuu  aaaaaaaaaa  aaaa   cccccccccccccccckkkkkkkk    kkkkkkk
+                                                                                                   Q:::::Q                                                                         
+                                                                                                    QQQQQQ                                                                                                                                                                                                                                                       
      AutoQuack - Automated Payload Generator
      Version 1.0.0 | Developed by Piyush R.
      https://github.com/PIYUSH-R04/AutoQuack.git
